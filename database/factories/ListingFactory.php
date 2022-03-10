bd6b7102486352750b38d4c18a542f96631ecc42
Vladimir <vovakovalchuk167@gmail.com>
2022-03-10 18:17:48 +0300

    added seeding data information

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence(rand(5, 7));
+        $slug = Str::slug($title) . '-' . rand(1111, 9999);
+        $logo = basename($this->faker->image(storage_path('app/public')));
+        $isHightLighted = (rand(1, 9) > 7);
+        $dateTime = $this->faker->dateTimeBetween('-1 month', 'now');
+
+        $content = '';
+
+        for ($i = 0; $i < 5; $i++) {
+            $content .= '<p class="mb-4">' . $this->faker->sentences(rand(5, 10), true) . '</p>';
+        }
+
+        return [
+            'title' => $title,
+            'slug' => $slug,
+            'company' => $this->faker->company,
+            'location' => $this->faker->country,
+            'logo' => $logo,
+            'is_highlighted' => $isHightLighted,
+            'is_active' => true,
+            'content' => $content,
+            'apply_link' => $this->faker->url,
+            'created_at' => $dateTime,
+            'updated_at' => $dateTime,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+//         \App\Models\User::factory(10)->create();
+//        Tag::factory(10)->create();
+//        Listing::factory(10)->create();
+
+        $tags = Tag::factory(10)->create();
+
+        User::factory(20)->create()->each(function ($user) use ($tags) {
+            Listing::factory(rand(1, 4))->create([
+                'user_id' => $user->id
+            ])->each(function ($listing) use ($tags) {
+                $listing->tags()->attach($tags->random(2));
+            });
+        });
     }
 }
